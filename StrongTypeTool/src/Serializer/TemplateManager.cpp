c00d1db3754401483704fb407f7fa3d8cd5f4c8b
DerWaschbar <bekagrdzelishvili0@gmail.com>
2022-02-28 10:59:34 +0400

    Add serializer for header only classes

--- StrongTypeTool/src/Serializer/TemplateManager.cpp
+++ StrongTypeTool/src/Serializer/TemplateManager.cpp
@@ -9,6 +9,7 @@ boost::filesystem::path getFileName(Template aTemplate) {
     switch(aTemplate) {
         case Template::T_Class: return "T_Class.txt";
         case Template::T_ClassHeader: return "T_ClassH.txt";
+        case Template::T_ClassHeaderOnly: return "T_ClassHOnly.txt";
         case Template::T_BinaryAssignmentOp: return "T_BinaryAssignmentOp.txt";
         case Template::T_BinaryAssignmentOpHeader: return "T_BinaryAssignmentOpH.txt";
         case Template::T_BinaryOp: return "T_BinaryOp.txt";
