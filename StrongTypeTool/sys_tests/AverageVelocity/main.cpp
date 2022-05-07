3abcd89f46fa9856f9a81825520ba759cf5ef485
DerWaschbar <bekagrdzelishvili0@gmail.com>
2022-05-07 15:24:48 +0400

    Fix average velocity system test

--- StrongTypeTool/sys_tests/AverageVelocity/main.cpp
+++ StrongTypeTool/sys_tests/AverageVelocity/main.cpp
@@ -2,22 +2,20 @@
 #include <vector>
 #include "StrongTypes/Distance.h"
 
-namespace example {
-
-    Velocity calculateAverageVelocity(std::vector<std::pair<Distance,Time>> data) {
-        Velocity velocitySum = Velocity(0.0);
-        for(std::pair<Distance, Time> entry : data) {
-            velocitySum += entry.first / entry.second;
-        }
-        return velocitySum / (float) data.size();
+Speed calculateAverageSpeed(std::vector<std::pair<Distance,Time>> data) {
+    Distance distanceSum = Distance(0.0);
+    Time timeSum = Time(0.0);
+    for(std::pair<Distance, Time> entry : data) {
+        distanceSum += entry.first;
+        timeSum += entry.second;
     }
-
+    return distanceSum / timeSum;
 }
 
 int main() {
-    std::cout << example::calculateAverageVelocity(
-            {{Distance(60.0),Time(1.0)},
-             {Distance(100.0),Time(2.0)}}).get() << std::endl;
+    std::cout << calculateAverageSpeed(
+             {{Distance(60.0),Time(1.0)},
+              {Distance(100.0),Time(2.0)}}).get() << std::endl;
 
     return 0;
 }
